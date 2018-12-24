cd5224601cfe4d2b856935cc04dd7b2f0b364ad9
邵励治 <shaolizhi1234@126.com>
2018-12-24 20:05:29 +0800

    Page 的生命周期函数实验

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -5,11 +5,43 @@ Page({
   data: {
     logs: []
   },
-  onLoad: function () {
+  onLoad: function() {
+    console.log("onLoad")
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
       })
     })
+  },
+  onShow: function() {
+    console.log("onShow")
+  },
+  onReady: function() {
+    console.log("onReady")
+  },
+  onHide: function() {
+    console.log("onHide")
+  },
+  onUnload: function() {
+    console.log("onUnload")
+  },
+  onPullDownRefresh: function() {
+    console.log("onPullDownRefresh")
+  },
+  onReachBottom: function() {
+    console.log("onReachBottom")
+  },
+  onShareAppMessage: function() {
+    console.log("onShareAppMessage")
+  },
+  onPageScroll: function() {
+    console.log("onPageScroll")
+  },
+  onResize: function() {
+    console.log("onResize")
+  },
+  onTabItemTap: function() {
+    console.log("onTabItemTap")
   }
+
 })
