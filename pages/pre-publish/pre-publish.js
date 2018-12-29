67c7d608357809c0f924dffbc99d15b71734df1d
邵励治 <shaolizhi1234@126.com>
2018-12-29 13:02:23 +0800

    init

--- pages/pre-publish/pre-publish.js
+++ pages/pre-publish/pre-publish.js
@@ -1,80 +1,66 @@
-// pages/test.js
+// pages/pre-publish/pre-publish.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    text: "邵励治喜欢陈邱爽",
-    fuck_array: [{
-      name: "邵励治"
-    }, {
-      name: "陈邱爽"
-    }]
-  },
-  like: function() {
-    this.setData({
-      text: "Yeah!!!"
-    })
-    console.log("yeah!!!")
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
-    return {
-      title: "我爱你，亲爱的大邱",
-      path: "/pages/test/test"
-    }
+  onShareAppMessage: function () {
+
   }
 })
