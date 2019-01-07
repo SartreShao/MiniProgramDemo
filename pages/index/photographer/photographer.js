5da0cf8a48c51556ddbf687f1a1a534a9dacdbd8
邵励治 <shaolizhi1234@126.com>
2019-01-07 21:23:21 +0800

    index 完成请求 API
    还差列表渲染就 OJBK

--- pages/index/photographer/photographer.js
+++ pages/index/photographer/photographer.js
@@ -1,5 +1,23 @@
 // pages/index/photographer/photographer.js
 Component({
+  lifetimes: {
+    attached() {
+      wx.request({
+        url: 'https://yuepai.leanapp.cn/graphql',
+        data: "{ Creation(containedIn: {publisherRole: [\"photographer\"]}) {objectId likedNumber createdAt creationOfPhoto(limit: 1) { objectId file { objectId url } } user { objectId nickName avatarUrl schoolName } } }",
+        header: {
+          'Content-Type': 'application/graphql'
+        },
+        method: "POST",
+        success: response => {
+          console.log(response.data)
+          this.setData({
+            creationList: JSON.stringify(response.data.data.Creation)
+          })
+        }
+      })
+    }
+  },
   /**
    * 组件的属性列表
    */
@@ -11,7 +29,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    creationList: []
   },
 
   /**
