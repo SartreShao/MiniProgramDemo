40eb5d75b3735d985fdbf532e0dff5b5692d7e85
邵励治 <shaolizhi1234@126.com>
2019-01-07 08:01:39 +0800

    滑动TAB事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,10 @@
 // pages/pre-publish/pre-publish.js
 Page({
   data: {
-    currentTab: "photographer"
+    currentTab: "photographer",
+    startX: 0,
+    startY: 0,
+    slipDirection: ""
   },
   // 点击事件：TAB - 摄影师
   clickTabPhotographer(event) {
@@ -15,6 +18,42 @@ Page({
       currentTab: "model"
     })
   },
+  // 滑动事件：起始
+  touchStartEvent(event) {
+    this.setData({
+      startX: event.changedTouches[0].clientX,
+      startY: event.changedTouches[0].clientY
+    })
+  },
+  // 滑动事件：结束
+  touchEndEvent(event) {
+    let endX = event.changedTouches[0].clientX;
+    let endY = event.changedTouches[0].clientY;
+    const getSlipDirection = (endX, endY, startX, startY) => {
+      let slipDirection = "";
+      if (endX - startX > 50 && Math.abs(endY - startY) < 50) {
+        // 右滑
+        slipDirection = "right";
+      } else if (endX - startX < -50 && Math.abs(endY - startY) < 50) {
+        // 左滑
+        slipDirection = "left";
+      }
+      return slipDirection;
+    }
+    this.setData({
+      slipDirection: getSlipDirection(endX, endY, this.data.startX, this.data.startY)
+    })
+    if (this.data.slipDirection === "right") {
+      this.setData({
+        currentTab: "photographer"
+      })
+    } else if (this.data.slipDirection === "left") {
+      this.setData({
+        currentTab:"model"
+      })
+   }
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
