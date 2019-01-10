22d990a25159978cf3f55e9064a770b03c7850cd
邵励治 <shaolizhi1234@126.com>
2019-01-10 20:48:46 +0800

    完成了 user 的逻辑层部分

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,82 +1,160 @@
 // pages/user/user.js
 // Get App's Instance
-const app = getApp()
+const app = getApp();
+const AV = require('../../libs/av-weapp-min.js');
 
-Page({
-  data: {
-    // 微信用户信息：微信名、性别、头像
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    // 获取全局变量中是否有 UserInfo，如果有意味着其已登录
-    if (app.globalData.userInfo) {
+function getUserInfo() {
+  if (app.globalData.userInfo) {
+    this.setData({
+      userInfo: app.globalData.userInfo,
+      hasUserInfo: true
+    })
+  } else if (this.data.canIUse) {
+    // 由于 getUserInfo 是网络请求，可能会在 Page。onLoad 之后才返回
+    // 所以在此处加入 callback 以防止这种情况
+    app.userInfoReadyCallback = res => {
       this.setData({
-        userInfo: app.globalData.userInfo,
+        userInfo: res.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page。onLoad 之后才返回
-      // 所以在此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    }
+  } else {
+    // 在没有 open-type=getUserInfo 版本的兼容处理
+    // 直接弹窗要求授权
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo;
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      // 直接弹窗要求授权
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-    const AV = require('../../libs/av-weapp-min.js');
+    })
+  }
+}
 
-    //获取 LeanCloud 中的数据
+function saveUserInfoToLeanCloud() {
+  console.log(AV.User.current().toJSON().nickName);
+  if (app.globalData.userInfo) {
     //LeanCloud 中用户名为空？
-    if (AV.User.current().toJSON().nickName == "") {
-      console.log("LeanCloud's nickName is null, put wechat's nickName in it")
-      const currentUser = AV.User.current()
-      currentUser.set("nickName", app.globalData.userInfo.nickName)
-      currentUser.save()
+    if (AV.User.current().toJSON().nickName === "" || AV.User.current().toJSON().nickName == null) {
+      console.log("LeanCloud's nickName is null, put wechat's nickName in it");
+      const currentUser = AV.User.current();
+      currentUser.set("nickName", app.globalData.userInfo.nickName);
+      currentUser.save();
       app.globalData.leanCloudUser = currentUser.toJSON()
     }
     //LeanCloud 中头像为空？
-    if (AV.User.current().toJSON().avatarUrl == "") {
-      console.log("LeanCloud's avatar is null, put wechat's avatar in it")
-      const currentUser = AV.User.current()
-      currentUser.set("avatarUrl", app.globalData.userInfo.avatarUrl)
-      currentUser.save()
+    if (AV.User.current().toJSON().avatarUrl === "" || AV.User.current().toJSON().avatarUrl == null) {
+      console.log("LeanCloud's avatar is null, put wechat's avatar in it");
+      const currentUser = AV.User.current();
+      currentUser.set("avatarUrl", app.globalData.userInfo.avatarUrl);
+      currentUser.save();
       app.globalData.leanCloudUser = currentUser.toJSON()
     }
-    if (AV.User.current().toJSON().sex == "") {
-      console.log("LeanCloud's sex is null, put wechat's avatar in it")
-      const currentUser = AV.User.current()
-      if (app.globalData.userInfo.gender == 1) {
-        currentUser.set("sex", "男")
+    if (AV.User.current().toJSON().sex === "" || AV.User.current().toJSON().sex == null) {
+      console.log("LeanCloud's sex is null, put wechat's avatar in it");
+      const currentUser = AV.User.current();
+      if (app.globalData.userInfo.gender === 1) {
+        currentUser.set("sex", "男");
         currentUser.save()
       } else {
-        currentUser.set("sex", "女")
+        currentUser.set("sex", "女");
         currentUser.save()
       }
     }
+  }
+}
+
+function getHasRoleAndSchool() {
+  wx.request({
+    url: 'https://yuepai.leanapp.cn/graphql',
+    data: "{\n" +
+      "  _User(objectId:\"" + AV.User.current().toJSON().objectId + "\") {\n" +
+      "    objectId\n" +
+      "    nickName\n" +
+      "    schoolName\n" +
+      "    usersOf_Role {\n" +
+      "      objectId\n" +
+      "      name\n" +
+      "    }\n" +
+      "  }\n" +
+      "}\n",
+    header: {
+      'Content-Type': 'application/graphql'
+    },
+    method: "POST",
+    success: response => {
+      console.log("fuck");
+      console.log(response.data);
+      if (response.data.data._User[0].schoolName == null || response.data.data._User[0].schoolName === "") {
+        //学校为空
+        this.setData({
+          hasRoleAndSchool: false
+        })
+      } else if (response.data.data._User[0].usersOf_Role.length === 0) {
+        //角色为空
+        this.setData({
+          hasRoleAndSchool: false
+        })
+      } else {
+        //二者皆不为空
+        this.setData({
+          hasRoleAndSchool: true
+        })
+      }
+      console.log("hasRoleAndSchool: " + this.data.hasRoleAndSchool)
+    }
+  });
+}
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    hasRoleAndSchool: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    /*
+     一：判断用户是否登录。
+        登录-> userInfo!=null；
+        未登录-> userInfo==null；
+        视图根据 userInfo 渲染不同的界面：
+        （1）登录->用户信息界面
+        （2）未登录->登录界面
+    */
+
+    // 获取全局变量中是否有 UserInfo，如果有意味着其已登录
+    getUserInfo.call(this);
+
+    /*
+      二：判断 LeanCloud 中是否有「用户身份」与「学校」
+        如果有的话，hasRoleAndSchool=true;
+        如果没有的话，hasRoleAndSchool=false;
+        根据 hasRoleAndSchool 的值，进行条件渲染：
+        true-> 显示学校与用户角色
+        false-> 显示“这里什么都没有”与“请完善个人信息”
+    */
+    getHasRoleAndSchool.call(this);
+
+    /*
+      三：向 LeanCloud 存用户头像、昵称、性别
+    */
+
+    //获取 LeanCloud 中的数据
+    saveUserInfoToLeanCloud();
+
   },
 
   // 获取微信用户信息成功回调
-  getUserInfoSuccessCallback: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  getUserInfoSuccessCallback: function (e) {
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
@@ -86,49 +164,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
-})
+});
